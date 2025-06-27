62141740852c8db5e775a01016a864f103092978
Marcin Gieroń <mgieron2012@gmail.com>
2025-06-28 00:43:36 +0200

    cloud

--- growie_django/settings.py
+++ growie_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z2e)u)&tit#hxlu_mq$+tvfhwn_y=-0qd_%5pr=j3+@fdy!n#('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
